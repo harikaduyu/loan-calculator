e2371bae669a14fa40074a506ab319db7186066f
harikaduyu <h.duyu@rebuy.com>
2020-05-24 20:36:36 +0200

    loader and user experience

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'block';
+  setTimeout(calculateResults,2000);
+  e.preventDefault();
+});
 
-function calculateResults(e){
+function calculateResults(){
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
   const years = document.getElementById('years');
@@ -19,11 +24,13 @@ function calculateResults(e){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly*calculatedPayments)-principal).toFixed(2);
+
+    document.getElementById('results').style.display = 'block';
+    
   } else {
     showError('Please check your numbers.');
   }
-
-  e.preventDefault();
+    document.getElementById('loading').style.display = 'none';
 }
 
 function showError(error){
